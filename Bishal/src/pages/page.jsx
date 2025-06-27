553d862ffaae795ace2492daf0198eb74c2581b7
iAmBotOkkk <barmanbishal1234@gmail.com>
2025-06-27 09:28:51 +0530

    Added section

---
+++ Bishal/src/pages/page.jsx
@@ -0,0 +1,13 @@
+
+
+const home = () => {
+    return (
+        <div className="homePage">
+            <h1>Bishal</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus vitae consequuntur vero. Culpa quia, repellendus quae totam voluptas tempora enim quaerat asperiores nisi voluptatibus.
+           Veniam officiis asperiores minima repellendus omnis?</p>
+        </div>
+    )
+}
+
+export default home;

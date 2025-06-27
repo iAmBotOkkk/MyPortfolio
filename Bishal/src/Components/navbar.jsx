75286b0d97732337e238d3cb4be9374d0a756547
iAmBotOkkk <barmanbishal1234@gmail.com>
2025-06-27 09:16:15 +0530

    Add components

---
+++ Bishal/src/Components/navbar.jsx
@@ -0,0 +1,18 @@
+import {Navlink} from "react-router-dom"
+
+ const Navbar = () => {
+return (
+    <nav className="navbar">
+    <div className="nav-link">
+    <Navlink to ="/home" className={({isActive}) =>isActive ? "logo-active" : "logo-inactive" }><h1>Bishal</h1></Navlink>
+    <Navlink to = "/home" className = {({isActive}) =>isActive ? "home-active" : "home-inactive"}>home</Navlink>
+    <Navlink to = "/about" className ={({isActive}) => isActive ? "about-active" : "about-inactive"}>about</Navlink>
+    
+    </div>
+    </nav>   
+)
+}
+
+export default Navbar;
+
+

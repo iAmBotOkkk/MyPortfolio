b62a58f897a76441e46b4b6c9abf25344e05f69a
iAmBotOkkk <barmanbishal1234@gmail.com>
2025-06-28 16:33:50 +0530

    Updated Css

--- Bishal/src/pages/page.jsx
+++ Bishal/src/pages/page.jsx
@@ -2,9 +2,11 @@
 
 const Page = () => {
     return (
+        
         <section>
         <div className="homePage">
-            <h1>Bishal</h1>
+             <h3> Hii I am,</h3>
+            <h1>👋🏻Bishal</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus vitae consequuntur vero. Culpa quia, repellendus quae totam voluptas tempora enim quaerat asperiores nisi voluptatibus.
            Veniam officiis asperiores minima repellendus omnis?</p>
            </div>
@@ -14,8 +16,8 @@ const Page = () => {
              web experiences.I specialize in building modern, scalable web apps using M.E.R.N stack 
             </p>
             </div>
-            
-            <form action="POST" className="contactForm">
+            <div className="contactForm">
+            <form action="POST" >
                 <h2>Let's Connect</h2>
                 <input type="text" placeholder="Name" required  />
                 <input type="text" placeholder="email" required />
@@ -23,9 +25,12 @@ const Page = () => {
 
                 <button>Send Message</button>
             </form>
-      
-        
+            </div>
+            <footer>
+                all copyrights reserved
+            </footer>
         </section>
+        
     )
 }
 

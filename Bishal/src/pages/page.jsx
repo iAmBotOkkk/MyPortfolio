6969d5a3f58524def415752d96358892ad77311f
iAmBotOkkk <barmanbishal1234@gmail.com>
2025-06-28 22:41:33 +0530

    updated Css

--- Bishal/src/pages/page.jsx
+++ Bishal/src/pages/page.jsx
@@ -7,11 +7,17 @@ const Page = () => {
             <div className="homePage">
                 <h1>Hii, I am Bishal👋🏻</h1>
                 <p>FullStack Developer that loves
-                   building cool stuffs and solving 
-                   <div> real world problems.Strong believer in clean code and good coffee☕</div> 
+                    building cool stuffs and solving
+                    <div> real world problems.Strong believer in clean code and good coffee☕</div>
                 </p>
             </div>
-            <div> <img src="" alt="" /></div>
+            <div className="images">
+                <img src="React.png" alt="" />
+                <img src="express.png" alt="" />
+                <img src="mongodb.png" alt="" />
+                <img src="nodeJS.png" alt="" />
+            </div>
+
             <div className="aboutPage">
                 <h2>About Me</h2>
                 <p>Hey I am Bishal,a fullstack developer who thrives on turning ideas into real,functional
@@ -19,7 +25,7 @@ const Page = () => {
                 </p>
             </div>
             <div className="contactForm">
-                <form action="POST" >
+                <form>
                     <h2>Let's Connect</h2>
                     <input type="text" placeholder="Name" required />
                     <input type="text" placeholder="email" required />
